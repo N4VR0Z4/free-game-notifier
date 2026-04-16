3e33ba8e828a77a600670a09e25094fa6d9e605e
ƒ <otikonavrozashvili@gmail.com>
2026-04-16 23:53:48 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+require('dotenv').config();
+const http = require("http");
+
+const WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
+const API_URL = process.env.API_URL;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Giveaway Bot is running!');
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+async function sendGiveaway() {
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+
+        if (!data || data.length === 0) return;
+
+        console.log(data);
+
+        const {id,title,worth,status,type,platforms,open_giveaway,thumbnail,image,description,instructions} = data[0];
+
+        const embed = {
+            title: `New Giveaway: ${title}`,
+            url: open_giveaway,
+            description: description,
+            color: 15844367,
+            thumbnail: {
+                url: thumbnail
+            },
+            image: {
+                url: image
+            },
+            fields: [
+                {
+                    name: "Instructions",
+                    value: instructions,
+                    inline: false
+                },
+                {
+                    name: "Worth",
+                    value: `~~${worth}~~ $0.00`,
+                    inline: true
+                },
+                {
+                    name: "Platforms",
+                    value: platforms,
+                    inline: true
+                },
+                {
+                    name: "Status",
+                    value: status,
+                    inline: true
+                },
+                {
+                    name: "Type",
+                    value: type,
+                    inline: true
+                }
+            ],
+        }
+
+        await fetch(WEBHOOK_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                embeds: [embed]
+            })
+        });
+
+        console.log('Giveaway sent to Discord successfully!');
+
+    } catch (error) {
+        console.error('Error fetching giveaway or sending to Discord:', error);
+    }
+}
+
+setInterval(sendGiveaway,60000)
